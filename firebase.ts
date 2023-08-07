ca759636e197c67169c4f97e63f557fe46f580f5
Ishaan <ishaan2053@gmail.com>
2023-08-07 15:15:41 +0530

    Added lots of files!

---
+++ firebase.ts
@@ -0,0 +1,18 @@
+import { getFirestore } from "firebase/firestore";
+import { getApp, getApps, initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAl61ltfCW61fUw4CKr4q8jbjKrKSCMQvE",
+  authDomain: "ishaan-34e82.firebaseapp.com",
+  databaseURL: "https://ishaan-34e82.firebaseio.com",
+  projectId: "ishaan-34e82",
+  storageBucket: "ishaan-34e82.appspot.com",
+  messagingSenderId: "714032130614",
+  appId: "1:714032130614:web:867875fa0acfd4cea18697"
+};
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+const db = getFirestore(app)
+export {db};
